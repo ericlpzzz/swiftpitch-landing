1319a983d5cf3a829ab81cee56ebabc1400db584
ericlpzzz <elopezbaltrons@gmail.com>
2025-07-02 18:55:22 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#007c91"
+      }
+    }
+  },
+  plugins: []
+};
